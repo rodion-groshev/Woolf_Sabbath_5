ba98dbe36373c35415eb38de387ff98a521e23fd
Rodion <rodion.groshev@gmail.com>
2023-12-19 15:20:07 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name:
+    pass
+
+
+class Phone:
+    pass
+
+
+class Email:
+    pass
+
+
+class Birthday:
+    pass
+
+
+class Address:
+    pass
+
+
+class Record:
+    pass
+
+
+class AddressBook:
+    pass
