ac74f83431ad3e99552953a4d0cf1b68262571b6
Shumakher Eduard <wendalin@protonmail.ch>
2023-12-20 15:10:07 +0100

    addressBook

--- main.py
+++ main.py
@@ -111,8 +111,137 @@ class Record:
             raise BadBirthdayFormat("Invalid birthday format. Expected format: dd/mm/yyyy.")
         
 
+
 class AddressBook:
-    pass
+    def __init__(self):
+        self.contacts = {}
+        
+    def add_contact(self, name, address, phone, email, birthday):
+        contact = Record(name)
+        contact.add_address(address)
+        contact.add_phone(phone)
+        contact.add_email(email)
+        contact.add_birthday(birthday)
+        self.contacts[name] = contact
+
+    def view_contact(self, name):
+        if name in self.contacts:
+            print(self.contacts[name])
+        else:
+            print(f"Contact '{name}' not found.")
+
+    def find_contact(self, query):
+        matches = [name for name in self.contacts if re.search(query, name, re.IGNORECASE)]
+        if matches:
+            print(f"\nMatching contacts for '{query}':")
+            for match in matches:
+                print(self.contacts[match])
+        else:
+            print(f"No contacts found for '{query}'.")
+
+    def show_all(self):
+        if self.contacts:
+            print("\nAll Contacts:")
+            for name, contact in self.contacts.items():
+                print(f"\n{name}:\n{contact}")
+        else:
+            print("No contacts in the address book.")
+
+    def show_contact(self, name):
+        if name in self.contacts:
+            contact = self.contacts[name]
+            print(f"\n{name}:\n{contact}")
+        else:
+            print(f"Contact '{name}' not found.")
+
+    def show_phone(self, name):
+        if name in self.contacts:
+            contact = self.contacts[name]
+            if contact.phones:
+                print(f"\n{name}'s Phone Numbers:")
+                for phone in contact.phones:
+                    print(phone)
+            else:
+                print(f"{name} has no phone numbers.")
+        else:
+            print(f"Contact '{name}' not found.")
+
+    def show_email(self, name):
+        if name in self.contacts:
+            contact = self.contacts[name]
+            if contact.email:
+                print(f"\n{name}'s Email:")
+                print(contact.email)
+            else:
+                print(f"{name} has no email address.")
+        else:
+            print(f"Contact '{name}' not found.")
+
+    def show_address(self, name):
+        if name in self.contacts:
+            contact = self.contacts[name]
+            if contact.address:
+                print(f"\n{name}'s Address:")
+                print(contact.address)
+            else:
+                print(f"{name} has no address.")
+        else:
+            print(f"Contact '{name}' not found.")
+
+    def show_birthday(self, name):
+        if name in self.contacts:
+            contact = self.contacts[name]
+            if contact.birthday:
+                print(f"\n{name}'s Birthday:")
+                print(contact.birthday)
+            else:
+                print(f"{name} has no birthday.")
+        else:
+            print(f"Contact '{name}' not found.")
+
+    def upcoming_birthday(self, days):
+        today = datetime.now()
+        upcoming_birthdays = []
+
+        for name, contact in self.contacts.items():
+            if contact.birthday:
+                birthday = contact.birthday.date
+                next_birthday = datetime(today.year, birthday.month, birthday.day)
+
+                if next_birthday < today:
+                    next_birthday = datetime(today.year + 1, birthday.month, birthday.day)
+
+                days_until_birthday = (next_birthday - today).days
+
+                if 0 < days_until_birthday <= days:
+                    upcoming_birthdays.append((name, contact.birthday))
+
+        return upcoming_birthdays
+
+    def delete_contact(self, name):
+        if name in self.contacts:
+            del self.contacts[name]
+            print(f"Contact '{name}' deleted successfully.")
+        else:
+            print(f"Contact '{name}' not found.")
+
+    def upcoming_birthdays(self, days):
+        today = datetime.now()
+        upcoming_birthdays = []
+
+        for name, contact in self.contacts.items():
+            birthday = contact.birthday.date
+            next_birthday = datetime(today.year, birthday.month, birthday.day)
+
+            if next_birthday < today:
+                next_birthday = datetime(today.year + 1, birthday.month, birthday.day)
+
+            days_until_birthday = (next_birthday - today).days
+
+            if 0 < days_until_birthday <= days:
+                upcoming_birthdays.append((name, contact.birthday))
+
+        return upcoming_birthdays
 
 
 def parse_input(user_input):
