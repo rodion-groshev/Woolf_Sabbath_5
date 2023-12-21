1ea074154db4a8fb6c7dfeca869b889bd9ff1be2
Rodion <rodion.groshev@gmail.com>
2023-12-21 04:31:56 +0100

    delete old files

--- name.py
+++
@@ -1,9 +0,0 @@
-# Class Name
-class Name:
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name.strip()
-        self.last_name = last_name.strip()
-        
-        if not self.first_name or not self.last_name:
-            raise ValueError("First and last names cannot be empty")
-        
