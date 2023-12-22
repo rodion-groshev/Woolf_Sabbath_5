78691843301b97b5bb6e4770722afe53c21b0939
renerroll <100288366+renerroll@users.noreply.github.com>
2023-12-22 20:07:13 +0200

    note_object

--- base_objects/val_inp.py
+++ base_objects/val_inp.py
@@ -1,17 +1,17 @@
-from base_objects.input_object import InputObj
+# from base_objects.input_object import InputObj
 
 
-class StringValueInp(InputObj):
-    def matches_query(self, gets_inp):
-        if gets_inp is not None:
-            if gets_inp.full_match:
-                if gets_inp.case_sensitive:
-                    return self.value == gets_inp.value
-                else:
-                    return self.value.lower() == gets_inp.value.lower()
-            else:
-                if gets_inp.case_sensitive:
-                    return gets_inp.value in self.value
-                else:
-                    return gets_inp.value.lower() in self.value.lower()
-        return True
+# class StringValueInp(InputObj):
+#     def matches_query(self, gets_inp):
+#         if gets_inp is not None:
+#             if gets_inp.full_match:
+#                 if gets_inp.case_sensitive:
+#                     return self.value == gets_inp.value
+#                 else:
+#                     return self.value.lower() == gets_inp.value.lower()
+#             else:
+#                 if gets_inp.case_sensitive:
+#                     return gets_inp.value in self.value
+#                 else:
+#                     return gets_inp.value.lower() in self.value.lower()
+#         return True
