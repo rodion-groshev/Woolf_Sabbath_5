65be633dd8c071f842ae88ad2959dea45152e625
Rodion <rodion.groshev@gmail.com>
2023-12-23 17:08:29 +0100

    rework commands

--- base_objects/commands_object.py
+++ base_objects/commands_object.py
@@ -9,16 +9,13 @@ from utilities.birtday_output import birthday_output
 
 class Commands:
     @input_error
-    def add_contact(self, book):
-        name = input("Enter the name: ")
-        surname = input("Enter the surname (optional): ")
+    def add_contact(self, name, book):
         phone = input("Enter the phone: ")
         email = input("Enter email: ")
         address = input("Enter the address: ")
         birthday = input("Enter a birthday: ")
-        if surname:
-            name = name + " " + surname
         record = Record(name)
+
         if phone:
             record.add_phone(phone)
         if email:
@@ -31,23 +28,15 @@ class Commands:
         return f"Contact {name} added successfully."
 
     @input_error
-    def add_phone(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            phone = args[-1]
-        else:
-            name, phone = args
+    def add_phone(self, name, book):
+        phone = input("Enter the phone number: ")
         record = book.find(name)
         record.add_phone(phone)
         return f"Phone: {phone} added to contact {name}."
 
     @input_error
-    def add_email(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            email = args[-1]
-        else:
-            name, email = args
+    def add_email(self, name, book):
+        email = input("Enter the phone number: ")
         record = book.find(name)
         email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
         if not re.match(email_pattern, email):
@@ -56,22 +45,15 @@ class Commands:
         return f"Email: {email} added to contact {name}."
 
     @input_error
-    def add_address(self, args, book):
-        name, *address_words = args
+    def add_address(self, name, book):
+        address = input("Enter the address: ")
         record = book.find(name)
-        address = ' '.join(address_words)
         record.add_address(address)
-
         return f"Address: {address} added to contact {name}."
 
     @input_error
-    def add_birthday(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            birthday = args[-1]
-        else:
-            name, birthday = args
-
+    def add_birthday(self, name, book):
+        birthday = input("Enter the phone number: ")
         record = book.find(name)
         try:
             datetime.strptime(birthday, "%d.%m.%Y")
@@ -81,32 +63,23 @@ class Commands:
         return f"Birthday: {birthday} added to contact {name}."
 
     @input_error
-    def edit_phone(self, args, book):
-        if len(args) == 4:
-            name = args[0] + " " + args[1]
-            old_phone = args[-2]
-            new_phone = args[-1]
-        else:
-            name, old_phone, new_phone = args
-
+    def edit_phone(self, name, book):
+        old_phone = input("Enter the old phone number: ")
+        new_phone = input("Enter the new phone number: ")
         if name in book:
             record = book.find(name)
             phone_pattern = re.compile(r'^\+38\d{9,10}$')
             if not re.match(phone_pattern, new_phone):
                 raise BadPhoneNumber(new_phone)
             record.edit_phone(old_phone, new_phone)
-            return "Contact updated."
+            return f"Contact {name} updated."
         else:
             return "Contact not found"
 
     @input_error
-    def edit_email(self, args, book):
-        if len(args) == 4:
-            name = args[0] + " " + args[1]
-            old_email = args[-2]
-            new_email = args[-1]
-        else:
-            name, old_email, new_email = args
+    def edit_email(self, name, book):
+        old_email = input("Enter the old email address: ")
+        new_email = input("Enter the new email address: ")
         if name in book:
             record = book.find(name)
             email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
@@ -118,23 +91,18 @@ class Commands:
             return "Contact not found"
 
     @input_error
-    def edit_address(self, args, book):
-        name, *address = args
+    def edit_address(self, name, book):
+        address = input("Enter the address: ")
         if name in book:
             record = book.find(name)
-            record.edit_address(*address)
+            record.edit_address(address)
             return "Contact updated."
         else:
             return "Contact not found"
 
     @input_error
-    def edit_birthday(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            birthday = args[-1]
-        else:
-            name, birthday = args
-
+    def edit_birthday(self, name, book):
+        birthday = input("Enter the birthday date: ")
         if name in book:
             record = book.find(name)
             try:
@@ -151,82 +119,54 @@ class Commands:
         return book.show_all()
 
     @input_error
-    def show_contact(self, args, book):
-        name = " ".join(args)
+    def show_contact(self, name, book):
         return book.show_contact(name)
 
     @input_error
-    def show_phone(self, args, book):
-        name = " ".join(args)
+    def show_phone(self, name, book):
         return book.show_phone(name)
 
     @input_error
-    def show_email(self, args, book):
-        name = " ".join(args)
+    def show_email(self, name, book):
         return book.show_email(name)
 
     @input_error
-    def show_address(self, args, book):
-        name = " ".join(args)
+    def show_address(self, name, book):
         return book.show_address(name)
 
     @input_error
-    def show_birthday(self, args, book):
-        name = " ".join(args)
+    def show_birthday(self, name, book):
         return book.show_birthday(name)
 
     @input_error
-    def delete_contact(self, args, book):
-        name = " ".join(args)
+    def delete_contact(self, name, book):
         return book.delete_contact(name)
 
     @input_error
-    def delete_phone(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            phone = args[-1]
-        else:
-            name, phone = args
-
+    def delete_phone(self, name, book):
+        phone = input("Enter the new phone number: ")
         record = book.find(name)
         return record.delete_phone(phone)
 
     @input_error
-    def delete_email(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            email = args[-1]
-        else:
-            name, email = args
-
+    def delete_email(self, name, book):
+        email = input("Enter the new phone number: ")
         record = book.find(name)
         return record.delete_email(email)
 
     @input_error
-    def delete_address(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            address = args[-1]
-        else:
-            name, address = args
-
+    def delete_address(self, name, book):
         record = book.find(name)
-        return record.delete_email(address)
+        return record.delete_address()
 
     @input_error
-    def delete_birthday(self, args, book):
-        if len(args) == 3:
-            name = args[0] + " " + args[1]
-            birthday = args[-1]
-        else:
-            name, birthday = args
-
+    def delete_birthday(self, name, book):
         record = book.find(name)
-        return record.delete_birthday(birthday)
+        return record.delete_birthday()
 
     @input_error
-    def upcoming_birthday(self, args, book):
-        days = args[0]
+    def upcoming_birthday(self, name, book):
+        days = name[0]
         birthday_output(book.upcoming_birthday(int(days)), book)
 
     @staticmethod
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from base_objects.commands_object import Commands
 from utilities.input_reader import parse_input
 from utilities.storage import Storage
-from input_reader import parse_input
 
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.shortcuts import CompleteStyle
@@ -23,6 +22,7 @@ def main():
         user_input = prompt('Enter the command: ', completer=word_completer,
                             complete_style=CompleteStyle.MULTI_COLUMN)
         command, *args = parse_input(user_input)
+        name = " ".join(args)
         class_command = Commands()
 
         if command == "exit":
@@ -30,49 +30,49 @@ def main():
             break
 
         elif command == "add-contact":
-            print(class_command.add_contact(book))
+            print(class_command.add_contact(name, book))
         elif command == "add-phone":
-            print(class_command.add_phone(args, book))
+            print(class_command.add_phone(name, book))
         elif command == "add-email":
-            print(class_command.add_email(args, book))
+            print(class_command.add_email(name, book))
         elif command == "add-address":
-            print(class_command.add_address(args, book))
+            print(class_command.add_address(name, book))
         elif command == "add-birthday":
-            print(class_command.add_birthday(args, book))
+            print(class_command.add_birthday(name, book))
         elif command == "edit-phone":
-            print(class_command.edit_phone(args, book))
+            print(class_command.edit_phone(name, book))
         elif command == "edit-email":
-            print(class_command.edit_email(args, book))
+            print(class_command.edit_email(name, book))
         elif command == "edit-address":
-            print(class_command.edit_address(args, book))
+            print(class_command.edit_address(name, book))
         elif command == "edit-birthday":
-            print(class_command.edit_birthday(args, book))
+            print(class_command.edit_birthday(name, book))
         elif command == "show-all":
             print(class_command.show_all(book))
         elif command == "show-contact":
-            print(class_command.show_contact(args, book))
+            print(class_command.show_contact(name, book))
         elif command == "show-phone":
-            print(class_command.show_phone(args, book))
+            print(class_command.show_phone(name, book))
         elif command == "show-email":
-            print(class_command.show_email(args, book))
+            print(class_command.show_email(name, book))
         elif command == "show-address":
-            print(class_command.show_address(args, book))
+            print(class_command.show_address(name, book))
         elif command == "show-birthday":
-            print(class_command.show_birthday(args, book))
+            print(class_command.show_birthday(name, book))
         elif command == "delete-contact":
-            print(class_command.delete_contact(args, book))
+            print(class_command.delete_contact(name, book))
         elif command == "delete-phone":
-            print(class_command.delete_phone(args, book))
+            print(class_command.delete_phone(name, book))
         elif command == "delete-email":
-            print(class_command.delete_email(args, book))
+            print(class_command.delete_email(name, book))
         elif command == "delete-address":
-            print(class_command.delete_address(args, book))
+            print(class_command.delete_address(name, book))
         elif command == "delete-birthday":
-            print(class_command.delete_birthday(args, book))
+            print(class_command.delete_birthday(name, book))
         elif command == "birthday":
-            class_command.upcoming_birthday(args, book)
+            class_command.upcoming_birthday(name, book)
         elif command == "help":
-            print(class_command.help())
+            class_command.help()
         else:
             print(f"Invalid command {command}.")
 
